4cbe7ff0548f4eece5558f6b7c9a9f90a10b03f6
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2023-03-25 12:41:21 +0300

    tagsApi and tagsApi typing

---
+++ src/api/tagsApi.ts
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { ITag } from "../types/tagTypes";
+
+export const tagsApi = createApi({
+  reducerPath: "tagsApi",
+  tagTypes: ["Tags"],
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
+  endpoints: (builder) => ({
+    getTags: builder.query<ITag[], void>({
+      query: () => `tags`,
+      providesTags: (result) => ["Tags"],
+    }),
+    addTags: builder.mutation<ITag, ITag>({
+      query: (body) => ({
+        url: `tags`,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: (result) => ["Tags"],
+    }),
+    updateTags: builder.mutation<ITag, ITag>({
+      query: (tag) => ({
+        url: `tags/${tag.id}`,
+        method: "PATCH",
+        body: tag,
+      }),
+      invalidatesTags: (result) => ["Tags"],
+    }),
+    deleteTags: builder.mutation<ITag, number>({
+      query: (id) => ({
+        url: `tags/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (result) => ["Tags"],
+    }),
+  }),
+});
+
+export const {
+  useGetTagsQuery,
+  useAddTagsMutation,
+  useUpdateTagsMutation,
+  useDeleteTagsMutation,
+} = tagsApi;
